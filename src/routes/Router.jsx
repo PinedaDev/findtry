6044f69702085ef2d389f577559873c817e364ea
abraham <abrahampineda818@gmail.com>
2022-12-22 18:04:46 +0200

    netlify error handeling test

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,10 +6,23 @@ import {
 import App from "../App";
 import Home from '../routes/Home'
 import CountryDetails from "./CountryDetails";
+import { useRouteError } from "react-router-dom";
+
+const ErrorBoundary = () => {
+    let error = useRouteError();
+    console.error(error);
+    // Uncaught ReferenceError: path is not defined
+    return (
+        <>
+            <App></App>
+        </>
+    );
+}
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<App />}>
+        <Route path="/" element={<App />}
+            errorElement={<ErrorBoundary />}>
             <Route index element={<Home />} />
             <Route path=":country" element={<CountryDetails />}></Route>
         </Route>
